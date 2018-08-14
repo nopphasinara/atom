21b2355f6316ff36ad5bea7415f8f1aa2b87b3a5
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-08-14 18:51:52 +0700

    Update source

--- packages/atom-autocomplete-php/php/tmp.php
+++ packages/atom-autocomplete-php/php/tmp.php
@@ -1,1 +1,1 @@
-<?php $config = array( 'composer' => '/usr/local/bin/composer', 'php' => '/usr/local/Cellar/php/7.2.5/bin/php', 'autoload' => array('vendor/autoload.php','autoload.php',), 'classmap' => array('vendor/composer/autoload_classmap.php','autoload/ezp_kernel.php',) );
+<?php $config = array( 'composer' => '/usr/local/bin/composer', 'php' => 'php', 'autoload' => array('vendor/autoload.php','autoload.php',), 'classmap' => array('vendor/composer/autoload_classmap.php','autoload/ezp_kernel.php',) );
