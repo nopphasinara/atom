b353a5c765e5d29db52ef2e42a8c49aca0da46d7
nopphasinara <nopphasin.arayasirikul@gmail.com>
2020-01-23 23:01:13 +0700

    update source

--- packages/Chromo-Color-Previews/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-## 1.6.0
-* Added support for gradients
-* Added support for HSL color codes
--- packages/Chromo-Color-Previews/spec/color-preview-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import ColorPreviewView from '../lib/color-preview-view';
-
-describe('ColorPreviewView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
