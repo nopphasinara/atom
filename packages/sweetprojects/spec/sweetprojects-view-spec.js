6177624140d6d590dc5f8ae8ab2dc61abe72fe24
nopphasinara <nopphasin.arayasirikul@gmail.com>
2020-06-07 11:08:27 +0700

    update source

--- packages/sweetprojects/spec/sweetprojects-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import SweetprojectsView from '../lib/sweetprojects-view';
-
-describe('SweetprojectsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
