e9469a0e3caa4410a868cda9b9ba59ee1ea1b527
nopphasinara <nopphasin.arayasirikul@gmail.com>
2020-06-08 13:38:54 +0700

    update source

--- packages/atom-js-console/spec/js-console-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import JsConsoleView from '../lib/js-console-view';
-
-describe('JsConsoleView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
