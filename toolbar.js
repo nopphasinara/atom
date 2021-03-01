c45f70b069bc613ad4ebafefb869933521a9035d
nopphasinara <nopphasin.arayasirikul@gmail.com>
2021-03-02 05:32:50 +0700

    Commit changes

--- toolbar.js
+++ toolbar.js
@@ -1,11 +1,47 @@
 module.exports = [
   {
-    type: "button",
-    icon: "gear",
-    callback: "flex-tool-bar:edit-config-file",
-    tooltip: "Edit Tool Bar",
+    type: "url",
+    iconset: "mdi",
+    icon: "git",
+    url: "https://github.com/{repo-owner}/{repo-name}",
+    tooltip: "Open on Github",
   },
+  { type: "spacer" },
   {
-    type: "spacer",
+    type: "button",
+    callback: [ "window:toggle-dev-tools" ],
+    tooltip: "Dev Tools",
+    icon: "console",
+    iconset: "mdi",
   },
+  // {
+  //   type: "url",
+  //   url: "https://github.com/{repo-owner}/{repo-name}",
+  //   tooltip: "Open on Github",
+  //   icon: "git",
+  //   iconset: "mdi",
+  // },
+  // {
+  //   type: "file",
+  //   file: "/README.md",
+  //   tooltip: "Open File",
+  //   icon: "file",
+  //   iconset: "mdi",
+  // },
+  // {
+  //   type: "function",
+  //   callback: function (editor) {
+  //     console.log(editor);
+  //     alert("fuck");
+  //   },
+  //   tooltip: "Run Function",
+  //   icon: "function",
+  //   iconset: "mdi",
+  // },
+  // {
+  //   type: "button",
+  //   icon: "columns",
+  //   iconset: "fa",
+  //   callback: [ "pane:split-right", "pane:split-right" ],
+  // },
 ];
