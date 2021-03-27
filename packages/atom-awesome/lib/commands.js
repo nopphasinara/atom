d984e10287f2974995f877a8a93f7848b78b76b6
nopphasinara <nopphasin.arayasirikul@gmail.com>
2021-03-27 12:57:47 +0700

    Commit changes

--- packages/atom-awesome/lib/commands.js
+++ packages/atom-awesome/lib/commands.js
@@ -7,53 +7,6 @@ let CommandManager = {
   registerCommands: function () {
     this.subscriptions = new CompositeDisposable();
     this.emitter = new Emitter();
-
-    atom.commands.add('atom-text-editor', 'custom:select-line', function () {
-      var editor = atom.workspace.getActiveTextEditor();
-      editor.mutateSelectedText(function (selection, i) {
-        selection.editor.selectLinesContainingCursors();
-
-        var selectedText = selection.editor.getSelectedText();
-        if (selectedText) {
-          var trimStart = selectedText.length - selectedText.trimStart().length;
-          var trimEnd = selectedText.length - selectedText.trimEnd().length;
-          var trimAll = selectedText.length - trimStart - trimEnd;
-          var buffer = selection.editor.getSelectedBufferRange();
-
-          if (selection.isReversed()) {
-            selection.editor.moveRight();
-            selection.editor.moveLeft(trimEnd);
-            selection.selectLeft(trimAll);
-          } else {
-            selection.editor.moveLeft();
-            selection.editor.moveRight(trimStart);
-            selection.selectRight(trimAll);
-          }
-        }
-      });
-
-      // var selections = editor.getSelections();
-      // for (var i = 0; i < selections.length; i++) {
-      //   var selection = selections[i];
-      //   var selectedText = selection.editor.getSelectedText();
-      //   if (selectedText) {
-      //     var trimStart = selectedText.length - selectedText.trimStart().length;
-      //     var trimEnd = selectedText.length - selectedText.trimEnd().length;
-      //     var trimAll = selectedText.length - trimStart - trimEnd;
-      //     var buffer = selection.editor.getSelectedBufferRange();
-      //
-      //     if (selection.isReversed()) {
-      //       selection.cursor.moveRight();
-      //       selection.cursor.moveLeft(trimEnd);
-      //       selection.cursor.selection.selectLeft(trimAll);
-      //     } else {
-      //       selection.cursor.moveLeft();
-      //       selection.cursor.moveRight(trimStart);
-      //       selection.cursor.selection.selectRight(trimAll);
-      //     }
-      //   }
-      // }
-    });
   },
 };
 
