7ab3aa5161576718deca269724c2dbbc20085e08
nopphasinara <nopphasin.arayasirikul@gmail.com>
2021-03-29 00:21:01 +0700

    Commit changes

--- packages/atom-awesome/lib/main.js
+++ packages/atom-awesome/lib/main.js
@@ -1,80 +1,44 @@
 'use strict';
 
-let AtomAwesome;
-let Provider;
 
-let atomAwesome = {
-  privateConfig: {},
-  config: {},
-  id: 'atom-awesome',
+const {
+  CompositeDisposable,
+  Disposable,
+} = require('atom');
 
-  requireFromPackage: function (moduleId) {
-    if (isNotUndefined(moduleId) && isString(moduleId)) {
-      var filepath = stdpath.join(this.getPackagePath(), moduleId);
-      if (isExistsSync(filepath)) {
-        return require(filepath);
-      }
-    }
-    return {};
-  },
 
-  getPackagePath: function () {
-    let filepath = fse.realpathSync(stdpath.join(atom.getLoadSettings().atomHome, 'packages', this.id));
-    return filepath;
-  },
+class AtomAwesome
+{
+  activate() {
+    this.subscriptions = new CompositeDisposable()
+    this.subscriptions.add(atom.commands.add('atom-workspace', {
+      'ascii-art:convert': function () {
+        this.convert();
+      },
+    }));
 
-  getPackageMainPath: function () {
-    let filepath = fse.realpathSync(stdpath.join(atom.getLoadSettings().atomHome, 'packages', this.id, 'lib'));
-    return filepath;
-  },
+    console.group('activate');
+    console.log(this);
+    console.groupEnd();
+  }
 
-  getAtomNodePath: function () {
-    let filepath = fse.realpathSync(stdpath.join(atom.getLoadSettings().atomHome, 'node_modules'));
-    return filepath;
-  },
+  deactivate() {
+    this.subscriptions.dispose();
 
-  setConfig: function () {
-    if (isNotUndefined(Provider) && isObject(Provider)) {
-      this.config = Provider.config;
-    }
-    this.privateConfig.packagePath = this.getPackagePath();
-    this.privateConfig.packageMainPath = stdpath.join(this.privateConfig.packagePath, 'lib/main.js');
-  },
+    console.group('deactivate');
+    console.log(this);
+    console.groupEnd();
+  }
 
-  registerCommands: function () {
-    let CommandManager = this.requireFromPackage('lib/commands.js');
-    CommandManager.registerCommands();
-  },
-
-  activate: function () {
-    console.log('activate atom-awesome');
-  },
-
-  getProvider: function() {
-    let fse = require('fs-extra');
-    let stdpath = require('path');
-    let _ = require('lodash');
-
-    global.fse = fse;
-    global.stdpath = stdpath;
-    global._ = _;
-
-    AtomAwesome = require(stdpath.join(this.getAtomNodePath(), 'atom-awesome/lib/main.js'));
-    AtomAwesome.getProvider();
-    Provider = require(stdpath.join(this.getPackagePath(), 'lib', 'provider.js'));
-
-    this.setConfig();
-
-    this.registerCommands();
-
-    return Provider;
-  },
+  convert() {
+    console.log('Convert text!');
+  }
 };
 
-console.clear();
+var atomAwesome = new AtomAwesome();
 
 console.group('atomAwesome');
 console.log(atomAwesome);
 console.groupEnd();
 
-module.exports = atomAwesome;
+exports.atomAwesome = atomAwesome;
