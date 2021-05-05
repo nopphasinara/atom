c5767637a8e75dd0801ee4a78c8b9c75b9f89aa6
nopphasinara <nopphasin.arayasirikul@gmail.com>
2021-05-06 03:56:52 +0700

    Commit changes

--- packages/atom-extended/index.js
+++ packages/atom-extended/index.js
@@ -2,89 +2,100 @@
 
 console.clear();
 
-var fs = require('fs');
-var _ = require('lodash');
-var path = require('path');
+if (atom.workspace.isTextEditor(atom.workspace.getActiveTextEditor())) {
+  let ACTIVE_EDITOR_PATH = atom.workspace.getActiveTextEditor().getPath();
 
-var _fs = new Object;
-console.log(typeof fs === typeof _fs);
+  var fs = require('fs');
+  var _ = require('lodash');
+  var path = require('path');
 
-// console.group('require');
-// console.log(fs);
-// console.log(fs.prototype || Object.getPrototypeOf(fs));
-// console.log(_);
-// console.log(_.prototype || Object.getPrototypeOf(_));
-// console.log(path);
-// console.log(path.prototype || Object.getPrototypeOf(path));
-// console.groupEnd();
+  var _fs = new Object;
+  console.log(typeof fs === typeof _fs);
 
-var utils = {
+  // module.loaded = false;
+  // module.load([path.dirname(ACTIVE_EDITOR_PATH), 'lib', 'config.js'].join('/'));
+  // var Config = module.exports;
+  // console.log(Config);
 
-  isTextEditor: function (object) {
-    if (typeof object === 'object') {
-      return atom.workspace.isTextEditor(object) || false;
-    }
-    return false;
-  },
+  
 
-  pathParse: function (filepath) {
-    if (typeof filepath !== 'string') {
+  // console.group('require');
+  // console.log(fs);
+  // console.log(fs.prototype || Object.getPrototypeOf(fs));
+  // console.log(_);
+  // console.log(_.prototype || Object.getPrototypeOf(_));
+  // console.log(path);
+  // console.log(path.prototype || Object.getPrototypeOf(path));
+  // console.groupEnd();
+
+  var utils = {
+
+    isTextEditor: function (object) {
+      if (typeof object === 'object') {
+        return atom.workspace.isTextEditor(object) || false;
+      }
+      return false;
+    },
+
+    pathParse: function (filepath) {
+      if (typeof filepath !== 'string') {
+        return;
+      }
+      return path.parse(filepath);
+    },
+
+    getDirname: function (filepath) {
+      if (typeof filepath !== 'string') {
+        return;
+      }
+      return this.pathParse(filepath).dir;
+    },
+
+    getBasename: function (filepath) {
+      if (typeof filepath !== 'string') {
+        return;
+      }
+      return this.pathParse(filepath).base;
+    },
+
+    getName: function (filepath) {
+      if (typeof filepath !== 'string') {
+        return;
+      }
+      return this.pathParse(filepath).name;
+    },
+
+    getExt: function (filepath) {
+      if (typeof filepath !== 'string') {
+        return;
+      }
+      return this.pathParse(filepath).ext;
+    },
+
+    getActiveTextEditor: function () {
+      var editor = atom.workspace.getActiveTextEditor();
+      const isTextEditor = this.isTextEditor(editor);
+      if (isTextEditor) {
+        return editor;
+      }
       return;
-    }
-    return path.parse(filepath);
-  },
+    },
 
-  getDirname: function (filepath) {
-    if (typeof filepath !== 'string') {
-      return;
-    }
-    return this.pathParse(filepath).dir;
-  },
+    getTextEditorPath: function (editor) {
+      if (typeof editor === 'undefined') {
+        return;
+      }
+      return (typeof fs === 'undefined') ? editor.getPath() : fs.realpathSync(editor.getPath());
+    },
 
-  getBasename: function (filepath) {
-    if (typeof filepath !== 'string') {
-      return;
-    }
-    return this.pathParse(filepath).base;
-  },
+  };
 
-  getName: function (filepath) {
-    if (typeof filepath !== 'string') {
-      return;
-    }
-    return this.pathParse(filepath).name;
-  },
+  const FILENAME = utils.getTextEditorPath(utils.getActiveTextEditor());
+  const DIRNAME = utils.getDirname(FILENAME);
+  const BASENAME = utils.getBasename(FILENAME);
 
-  getExt: function (filepath) {
-    if (typeof filepath !== 'string') {
-      return;
-    }
-    return this.pathParse(filepath).ext;
-  },
-
-  getActiveTextEditor: function () {
-    var editor = atom.workspace.getActiveTextEditor();
-    const isTextEditor = this.isTextEditor(editor);
-    if (isTextEditor) {
-      return editor;
-    }
-    return;
-  },
-
-  getTextEditorPath: function (editor) {
-    if (typeof editor === 'undefined') {
-      return;
-    }
-    return (typeof fs === 'undefined') ? editor.getPath() : fs.realpathSync(editor.getPath());
-  },
-
-};
-
-const FILENAME = utils.getTextEditorPath(utils.getActiveTextEditor());
-const DIRNAME = utils.getDirname(FILENAME);
-const BASENAME = utils.getBasename(FILENAME);
-
-console.group(BASENAME);
-console.log(DIRNAME);
-console.log(FILENAME);
-console.groupEnd();
+  console.group(BASENAME);
+  console.log(DIRNAME);
+  console.log(FILENAME);
+  console.groupEnd();
+}
---
+++ packages/filesize/lib/filesize-view.js
@@ -0,0 +1,183 @@
+'use babel';
+
+import { Disposable } from 'atom'; // eslint-disable-line
+
+import {
+  addImageInfo,
+  addMimeTypeInfo,
+  addPrettyDateInfo,
+  addPrettySize,
+  addGzipSize,
+} from 'filesize-calculator';
+
+function createContainer() {
+  const container = document.createElement('div');
+  container.className = 'file-size inline-block';
+  const tooltipLink = document.createElement('a');
+  tooltipLink.className = 'file-size-link';
+  const filesizeElement = document.createElement('span');
+  filesizeElement.className = 'current-size';
+  tooltipLink.appendChild(filesizeElement);
+  container.appendChild(tooltipLink);
+  return container;
+}
+
+function createTooltip(target, htmlContent) {
+  return atom.tooltips.add(target, {
+    template: `<div class="tooltip" role="tooltip">
+                 <div class="tooltip-arrow"></div>
+                 <div class="tooltip-outer">
+                   <div class="tooltip-inner" style="min-width: 36em; padding: .5em;"></div>
+                 </div>
+               </div>`,
+    title: htmlContent,
+    placement: 'top',
+    trigger: 'manual',
+    delay: 0,
+    animation: false,
+  });
+}
+
+function createInfoItem(label, info) {
+  return `<tr style='display: block; width: 100%; padding: 0; margin: 0;'>
+            <td style='width: 40%; padding: 10px 20px; margin: 0; display:
+             inline-block; text-align: right; vertical-align: middle;'>
+              ${label}
+            </td>
+            <td style='width: 50%; padding: 10px 20px; margin: 0; display:
+             inline-block; text-align: left; vertical-align: middle;'>
+              ${info}
+            </td>
+          </tr>`;
+}
+
+function getHTMLForTooltip(info) {
+  const {
+    prettySize,
+    gzipSize,
+    mimeType,
+    prettyDateCreated,
+    prettyDateChanged,
+    dimmensions,
+    absolutePath,
+    isRemote,
+  } = info;
+  const outer = document.createElement('div');
+  const content = document.createElement('div');
+  const infoContainer = document.createElement('table');
+
+  content.style.cssText = `width: 100%; display: block; margin: 0; padding: 0;
+                           background: inherit; border-radius: inherit;`;
+
+  infoContainer.style.cssText = `display: block; border: 0;
+                                 borderCollapse: collapse; padding: 0;
+                                 width: 100%; background: rgba(0,0,0,0.75); color: #DDD;
+                                 list-style: none; border-radius: inherit;
+                                 margin: 0 auto`;
+  let htmlItems = '';
+  if (isRemote) htmlItems += createInfoItem('Remote file', 'YES');
+  if (prettySize) htmlItems += createInfoItem('Size', prettySize);
+  if (gzipSize) htmlItems += createInfoItem('Gzipped size', gzipSize);
+  if (mimeType) htmlItems += createInfoItem('Mime type', mimeType);
+  if (prettyDateCreated) htmlItems += createInfoItem('Creation date', prettyDateCreated);
+  if (prettyDateChanged) htmlItems += createInfoItem('Last changed', prettyDateChanged);
+  if (dimmensions) {
+    htmlItems += createInfoItem('Dimmensions', `${dimmensions.width}x${dimmensions.height}`);
+  }
+
+  infoContainer.innerHTML = `<tbody style="display: block; width: 100%;">
+                              ${htmlItems}
+                             <tbody/>`;
+
+  const titleContainer = document.createElement('p');
+  titleContainer.style.fontSize = '1em';
+  const title = document.createTextNode(absolutePath);
+  titleContainer.appendChild(title);
+  content.appendChild(titleContainer);
+  content.appendChild(infoContainer);
+  outer.appendChild(content);
+  return outer.innerHTML;
+}
+
+function filesizeView(parent) {
+  let tooltip = null;
+  let tooltipListener = null;
+
+  function hideTooltip(event) {
+    if (event && event.target.className === '.tooltip') return true;
+    if (tooltip) tooltip.dispose();
+    tooltip = null;
+    parent.removeEventListener('click', hideTooltip, false);
+    return true;
+  }
+
+  function prepareTooltip(element, info, currentConfig) {
+    return (() => {
+      if (!tooltip) {
+        if (info.isRemote) {
+          tooltip = createTooltip(element, getHTMLForTooltip(info));
+        } else {
+          let content = addPrettyDateInfo(addImageInfo(addMimeTypeInfo(info)), currentConfig);
+          if (currentConfig.showGzippedSize) content = addGzipSize(content, currentConfig);
+          tooltip = createTooltip(element, getHTMLForTooltip(content));
+        }
+        setTimeout(() => {
+          parent.addEventListener('click', hideTooltip, false);
+        }, 0);
+      }
+      return false;
+    });
+  }
+
+  const self = {
+    container: createContainer(),
+    refresh(rawInfo) {
+      const config = {
+        useDecimal: atom.config.get('filesize.UseDecimal'),
+        use24HourFormat: atom.config.get('filesize.DisplayFullDayTimeOnPopup'),
+        showPopup: atom.config.get('filesize.EnablePopupAppearance'),
+        showGzippedSize: atom.config.get('filesize.DisplayGzippedSizeOnPopup'),
+      };
+      const filesizeElement = parent.querySelector('.current-size');
+      const statusInfo = addPrettySize(rawInfo, config);
+      if (filesizeElement) {
+        if (rawInfo.isRemote) {
+          filesizeElement.innerHTML = `~${statusInfo.prettySize}`;
+        } else {
+          filesizeElement.innerHTML = statusInfo.prettySize;
+        }
+      }
+      const showTooltip = prepareTooltip(filesizeElement, statusInfo, config);
+      hideTooltip();
+      if (tooltipListener) tooltipListener.dispose();
+      tooltipListener = null;
+      if (config.showPopup && !tooltip) {
+        const filesizeLink = parent.querySelector('.file-size-link');
+        filesizeLink.addEventListener('click', showTooltip, false);
+        tooltipListener = new Disposable(() => filesizeLink
+          .removeEventListener('click', showTooltip, false));
+      }
+      return self;
+    },
+
+    clean() {
+      hideTooltip();
+      const filesizeElement = parent.querySelector('.current-size');
+      if (filesizeElement) filesizeElement.innerHTML = '';
+      if (tooltipListener) tooltipListener.dispose();
+      tooltipListener = null;
+      return self;
+    },
+
+    destroy() {
+      hideTooltip();
+      self.clean();
+      self.container.parentNode.removeChild(self.container);
+      return self;
+    },
+  };
+
+  return self;
+}
+
+export default filesizeView;
